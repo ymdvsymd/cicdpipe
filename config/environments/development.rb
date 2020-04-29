d4f39e41a5cc3f3683bbc7ff0eb3a1ed0c33fbcb
chaser-at-its <tiw@outlook.jp>
2020-04-29 03:24:51 +0000

    railsの構成を変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "cicdpipe"
+  config.web_console.whitelisted_ips = ["172.16.0.0/12"]
 end
---
+++ config/initializers/blocked_ hosts.rb
@@ -0,0 +1,5 @@
+Rails.application.configure do
+  config.hosts << "cicdpipe"
+  config.hosts << "example.com"
+  config.hosts << "baukis2.example.com"
+end
