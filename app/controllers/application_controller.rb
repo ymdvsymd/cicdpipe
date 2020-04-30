e9020ff93d94079f9568437997ae722a4a9e1ee9
chaser-at-its <tiw@outlook.jp>
2020-04-30 18:21:18 +0900

    職員トップページのスタイルを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  private
+
+    def set_layout
+      if params[:controller] =~ %r{\A(staff|admin|customer)/}
+        Regexp.last_match[1]
+      else
+        "customer"
+      end
+    end
 end
