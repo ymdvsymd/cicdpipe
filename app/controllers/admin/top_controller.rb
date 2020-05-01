6db14ec10259e6f76e40977c2b4b70ecf5f54919
chaser-at-its <tiw@outlook.jp>
2020-05-01 02:58:16 +0000

    管理者、職員向けトップページのスタイルを追加

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,5 +1,3 @@
 class Admin::TopController < ApplicationController
-  def index
-
-  end
+  def index; end
 end
--- app/controllers/customer/top_controller.rb
+++ app/controllers/customer/top_controller.rb
@@ -1,4 +1,3 @@
 class Customer::TopController < ApplicationController
-  def index
-  end
+  def index; end
 end
