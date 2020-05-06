8354325b5013b157099b9a049a35baef5d55e9c9
chaser-at-its <tiw@outlook.jp>
2020-05-06 17:05:13 +0900

    マルチステージビルドの導入

--- docker/app/setup-production.sh
+++ docker/app/setup-production.sh
@@ -5,8 +5,6 @@ yarn install
 
 SECRET_KEY_BASE=for_assets_precompile bundle exec rails assets:precompile
 
-apt-get purge -y --auto-remove $TMP_DEPS $BUILD_DEPS && apt-get clean
-rm -rf ./node_modules /var/lib/apt/lists/* /tmp/* /var/tmp/*
-truncate -s 0 /var/log/*log
+rm -rf ./node_modules
 
 chmod +x ./entrypoint.sh
