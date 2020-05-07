0a2838b9d19d5b297015e3e44d265390979ff7d9
chaser-at-its <tiw@outlook.jp>
2020-05-07 22:04:04 +0900

    コンテナの設定を調整

--- docker/app/setup-test.sh
+++ docker/app/setup-test.sh
@@ -2,3 +2,8 @@
 
 BUNDLE_WITHOUT="development" bundle install
 NODE_ENV="production" yarn install
+
+SECRET_KEY_BASE=for_assets_precompile bundle exec rails assets:precompile
+
+apk del --purge $DEV_DEPS
+rm -rf /usr/local/share/.cache/yarn ${APP_ROOT}/node_modules
