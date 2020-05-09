039d54180fa6abd655154e43bf0add76146833bb
chaser-at-its <tiw@outlook.jp>
2020-05-09 23:50:46 +0900

    production用の構成を追加

--- config/initializers/baukis2.rb
+++ config/initializers/baukis2.rb
@@ -1,7 +1,15 @@
 Rails.application.configure do
-  config.baukis2 = {
-    staff: { host: "baukis2.example.com", path: "" },
-    admin: { host: "baukis2.example.com", path: "admin" },
-    customer: { host: "example.com", path: "mypage" }
-  }
+  config.baukis2 = if Rails.env = "development"
+                     {
+                       staff: { host: "baukis2.example.com", path: "" },
+                       admin: { host: "baukis2.example.com", path: "admin" },
+                       customer: { host: "example.com", path: "mypage" }
+                     }
+                   else
+                     {
+                       staff: { host: "alb-main-727819637.ap-northeast-1.elb.amazonaws.com", path: "" },
+                       admin: { host: "alb-main-727819637.ap-northeast-1.elb.amazonaws.com", path: "admin" },
+                       customer: { host: "alb-main-727819637.ap-northeast-1.elb.amazonaws.com", path: "mypage" }
+                     }
+                   end
 end
