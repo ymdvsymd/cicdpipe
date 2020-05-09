26c1f4b9d114f146574fd26bca505f78534882ae
chaser-at-its <tiw@outlook.jp>
2020-05-09 22:59:35 +0900

    dockerfileのエラー修正

--- docker/app/setup-production.sh
+++ docker/app/setup-production.sh
@@ -5,7 +5,7 @@ yarn install
 
 SECRET_KEY_BASE=for_assets_precompile bundle exec rails assets:precompile
 
-apk del --purge $DEV_DEPS
+apk del --purge $BUILD_DEPS
 rm -rf /usr/local/share/.cache/yarn ${APP_ROOT}/node_modules
 
 chmod +x ./docker/app/entrypoint.sh
--- docker/app/setup-test.sh
+++ docker/app/setup-test.sh
@@ -5,5 +5,5 @@ NODE_ENV="production" yarn install
 
 SECRET_KEY_BASE=for_assets_precompile bundle exec rails assets:precompile
 
-apk del --purge $DEV_DEPS
-rm -rf /usr/local/share/.cache/yarn ${APP_ROOT}/node_modules
+apk del --purge $BUILD_DEPS
+rm -rf ${APP_ROOT}/node_modules
