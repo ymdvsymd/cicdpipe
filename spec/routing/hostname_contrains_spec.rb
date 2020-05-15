0448d896b84e679811ce8d774ab82f0e564d79c0
chaser-at-its <tiw@outlook.jp>
2020-05-15 12:52:04 +0900

    環境によって動作が異なるためテスト環境、開発環境で実施するテストを変える

--- spec/routing/hostname_contrains_spec.rb
+++ spec/routing/hostname_contrains_spec.rb
@@ -31,11 +31,17 @@ describe "ルーティング" do
     )
   end
 
-  example "ホスト名が対象外ならrails/welcomeにルーティング" do
-    expect(get: "http://foobar.com").to route_to(
-      controller: "rails/welcome",
-      action: "index"
-    )
+  if Rails.env.development?
+    example "ホスト名が対象外ならrails/welcomeにルーティング" do
+      expect(get: "http://foobar.com").to route_to(
+        controller: "rails/welcome",
+        action: "index"
+      )
+    end
+  else
+    example "ホスト名が対象外ならroutableではない" do
+      expect(get: "http://foobar.com").not_to be_routable
+    end
   end
 
   example "存在しないパスならroutableではない" do
