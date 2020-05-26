27b39067af9ac1abe6758deff91315fe5d97f1b0
chaser-at-its <tiw@outlook.jp>
2020-05-26 22:08:31 +0900

    リファクタリング

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,5 +1,13 @@
 class StaffMemberPresenter < ModelPresenter
-  delegate :suspended?, to: :object
+  delegate :family_name, :given_name, :family_name_kana, :given_name_kana, :suspended?, to: :object
+
+  def full_name
+    "#{family_name} #{given_name}"
+  end
+
+  def full_name_kana
+    "#{family_name_kana} #{given_name_kana}"
+  end
 
   def suspended_mark
     suspended? ? raw("&#x2611;") : raw("&#x2610;")
