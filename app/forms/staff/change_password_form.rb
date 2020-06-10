dd61a26f1c4360d94d64c9a8af0054eb3559839f
chaser-at-its <tiw@outlook.jp>
2020-06-10 12:55:44 +0900

    個人電話番号、自宅電話番号の追加

--- app/forms/staff/change_password_form.rb
+++ app/forms/staff/change_password_form.rb
@@ -2,6 +2,7 @@ class Staff::ChangePasswordForm
   include ActiveModel::Model
 
   attr_accessor :object, :current_password, :new_password, :new_password_confirmation
+
   validates :new_password, presence: true, confirmation: true
 
   validate do
--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -8,10 +8,16 @@ class Staff::CustomerForm
   def initialize(customer = nil)
     @customer = customer
     @customer ||= Customer.new(gender: "male")
+    (2 - @customer.personal_phones.size).times do
+      @customer.personal_phones.build
+    end
     self.inputs_home_address = @customer.home_address.present?
     self.inputs_work_address = @customer.work_address.present?
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address
+    (2 - @customer.home_address.phones.size).times do
+      @customer.home_address.phones.build
+    end
   end
 
   def assign_attributes(params = {})
@@ -20,11 +26,33 @@ class Staff::CustomerForm
     self.inputs_work_address = params[:inputs_work_address] == "1"
 
     customer.assign_attributes(customer_params)
+    phones = phone_params(:customer).fetch(:phones)
+
+    customer.personal_phones.size.times do |index|
+      attributes = phones[index.to_s]
+      if attributes && attributes[:number].present?
+        customer.personal_phones[index].assign_attributes(attributes)
+      else
+        customer.personal_phones[index].mark_for_destruction
+      end
+    end
+
     if inputs_home_address
       customer.home_address.assign_attributes(home_address_params)
+      phones = phone_params(:home_address).fetch(:phones)
+
+      customer.home_address.phones.size.times do |index|
+        attributes = phones[index.to_s]
+        if attributes && attributes[:number].present?
+          customer.home_address.phones[index].assign_attributes(attributes)
+        else
+          customer.home_address.phones[index].mark_for_destruction
+        end
+      end
     else
       customer.home_address.mark_for_destruction
     end
+
     if inputs_work_address
       customer.work_address.assign_attributes(work_address_params)
     else
@@ -35,7 +63,7 @@ class Staff::CustomerForm
   private
 
     def customer_params
-      @params.require(:customer).permit(
+      @params.require(:customer).except(:phones).permit(
         :email, :password,
         :family_name, :given_name, :family_name_kana, :given_name_kana,
         :birthday, :gender
@@ -43,15 +71,19 @@ class Staff::CustomerForm
     end
 
     def home_address_params
-      @params.require(:home_address).permit(
+      @params.require(:home_address).except(:phones).permit(
         :postal_code, :prefecture, :city, :address1, :address2
       )
     end
 
     def work_address_params
-      @params.require(:work_address).permit(
+      @params.require(:work_address).except(:phones).permit(
         :postal_code, :prefecture, :city, :address1, :address2,
         :company_name, :division_name
       )
+    end
+
+    def phone_params(record_name)
+      @params.require(record_name).slice(:phones).permit(phones: [:number, :primary])
     end
 end
