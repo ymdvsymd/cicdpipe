b8aa7634bbe837adf608f39d18e66fe09871ccde
chaser-at-its <tiw@outlook.jp>
2020-06-13 23:22:19 +0900

    入力しない場合、住所を非表示に

--- app/javascript/staff/customer_form.js
+++ app/javascript/staff/customer_form.js
@@ -1,13 +1,19 @@
 function toggle_home_address_fields() {
   const checked = $("input#form_inputs_home_address").prop("checked");
-  $("fieldset#home-address-fields input").prop("disabled", !checked);
-  $("fieldset#home-address-fields select").prop("disabled", !checked);
+  change_visible("fieldset#home-address-fields", checked);
 }
 
 function toggle_work_address_fields() {
   const checked = $("input#form_inputs_work_address").prop("checked");
-  $("fieldset#work-address-fields input").prop("disabled", !checked);
-  $("fieldset#work-address-fields select").prop("disabled", !checked);
+  change_visible("fieldset#work-address-fields", checked);
+}
+
+function change_visible(query, visible) {
+  if (visible) {
+    $(query).show();
+  } else {
+    $(query).hide();
+  }
 }
 
 $(document).on("ready turbolinks:load", () => {
